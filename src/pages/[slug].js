a737c0f2c8ff2da2c08d31b23cdd4eedfeb9c40e
vinicdev <vinic8670@gmail.com>
2024-03-05 13:34:57 -0300

    feat: formatando código e alterando caminho favicon

--- src/pages/[slug].js
+++ src/pages/[slug].js
@@ -5,42 +5,48 @@ import Link from "next/link";
 import Head from "next/head";
 
 const Post = ({ date, markdown, title, slug }) => (
-    <>
-        <Head>
-            <title>{title}</title>
-            <link rel="canonical" href={`https://blog.vinic.dev/${slug}`} />
+  <>
+    <Head>
+      <title>{title}</title>
+      <link rel="canonical" href={`https://blog.vinic.dev/${slug}`} />
+    </Head>
 
-        </Head>
+    <article className={post.containter}>
+      <time>{date}</time>
+      <ReactMarkdown>{markdown}</ReactMarkdown>
 
-        <article className={post.containter}>
-            <time>{date}</time>
-            <ReactMarkdown>{markdown}</ReactMarkdown>
+      <hr />
 
-            <hr />
-
-            <p className={post.anuncio}>
-                Gostaria muito de saber sua opinião sobre esta publicação! Deixe um feedback em meu  <Link href="https://www.instagram.com/c4rdos0_/">Instagram</Link>, <Link href="https://www.linkedin.com/in/vinicdev/">LinkedIn</Link> ou no campo de comentário abaixo e compartilhe suas ideias ou sugestões. Espero que o conteúdo tenha sido útil para você e agregado valor ao seu conhecimento.
-                <br /><br />
-                Obrigado, até a próxima!
-            </p>
-        </article>
-    </>
+      <p className={post.anuncio}>
+        Gostaria muito de saber sua opinião sobre esta publicação! Deixe um
+        feedback em meu{" "}
+        <Link href="https://www.instagram.com/c4rdos0_/">Instagram</Link>,{" "}
+        <Link href="https://www.linkedin.com/in/vinicdev/">LinkedIn</Link> ou no
+        campo de comentário abaixo e compartilhe suas ideias ou sugestões.
+        Espero que o conteúdo tenha sido útil para você e agregado valor ao seu
+        conhecimento.
+        <br />
+        <br />
+        Obrigado, até a próxima!
+      </p>
+    </article>
+  </>
 );
 
 export const getStaticPaths = async () => {
-    const posts = await getPosts();
+  const posts = await getPosts();
 
-    return {
-        paths: posts.map((post) => `/${post.slug}`),
-        fallback: false,
-    };
+  return {
+    paths: posts.map((post) => `/${post.slug}`),
+    fallback: false,
+  };
 };
 
 export const getStaticProps = async ({ params: { slug } }) => {
-    const posts = await getPosts();
-    const post = posts.find((post) => post.slug === slug);
+  const posts = await getPosts();
+  const post = posts.find((post) => post.slug === slug);
 
-    return { props: post };
-}
+  return { props: post };
+};
 
 export default Post;
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,24 +1,36 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="pt-br">
       <Head>
         <link rel="canonical" href="https://blog.vinic.dev/" />
-        <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap" rel="stylesheet" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap"
+          rel="stylesheet"
+        />
         <link rel="icon" type="image/x-icon" href="/logo.png" />
-        <link rel="icon" href="/favicon.icon" />
-        <meta charset="UTF-8" />
-        <meta name="description" content="Aprimore suas habilidades em programação com o Cardoso Post - tutoriais, dicas e artigos de qualidade para iniciantes e especialistas. Confira agora!" />
+        <link rel="icon" href="/logo.png" />
+        <meta charSet="UTF-8" />
+        <meta
+          name="description"
+          content="Aprimore suas habilidades em programação com o Cardoso Post - tutoriais, dicas e artigos de qualidade para iniciantes e especialistas. Confira agora!"
+        />
         <meta name="author" content="Vinicius Cardoso" />
 
         {/* Social media meta */}
-        <meta property="og:description" content="Cardoso POST | O blog do programador" />
+        <meta
+          property="og:description"
+          content="Cardoso POST | O blog do programador"
+        />
         <meta property="og:image" content="/blog_cardoso_post.png" />
         <link href="/blog_cardoso_post.png" rel="image_src" />
         <meta property="og:site_name" content="Cardoso POST" />
-        <meta property="og:title" content="Cardoso POST | O blog do programador" />
+        <meta
+          property="og:title"
+          content="Cardoso POST | O blog do programador"
+        />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://blog.vinic.dev/" />
       </Head>
@@ -27,11 +39,12 @@ export default function Document() {
         <NextScript />
 
         {/* Google tag (gtag.js) */}
-        <Script strategy="afterInteractive" async src="https://www.googletagmanager.com/gtag/js?id=G-JZF158YLN1"></Script>
         <Script
-          id="gtag-init"
           strategy="afterInteractive"
-        >
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-JZF158YLN1"
+        ></Script>
+        <Script id="gtag-init" strategy="afterInteractive">
           {`
           
             window.dataLayer = window.dataLayer || [];
@@ -43,5 +56,5 @@ export default function Document() {
         </Script>
       </body>
     </Html>
-  )
+  );
 }
